83edc39911fac446efcf188684d719bf60ec1f33
Viannxo <eduardo.vmeira@gmail.com>
2024-10-06 13:50:33 -0300

    edit

---
+++ Lista1.2/sequencia3.c
@@ -0,0 +1,33 @@
+/* Implemente uma função que receba um inteiro N e retorne o resultado da seguinte expressão:
+   S = 1 + 1/1! + 1/2! + 1/3! + ... + 1/N!
+
+   Entrada: uma linha contendo o valor de N.
+   Saída: uma linha com o resultado da soma, com 2 casas decimais. */
+
+#include <stdio.h>
+
+float Sum(int n);
+int Fat(int n);
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    float result = Sum(n);
+    printf("%.2f\n", result);
+}
+
+float Sum(int n) {
+    float total = 0.0;
+    for (int i = 0; i <= n; i++) {
+        total += 1.0 / Fat(i);
+    }
+    return total;
+}
+
+int Fat(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * Fat(n - 1);
+    }
+}
