605e33927cdafc046ad9de9c3f3ecb7865399e46
Viannxo <eduardo.vmeira@gmail.com>
2024-11-07 15:44:51 -0300

    edit

--- labAEDS/Ativ2/SomaColuna.c
+++ labAEDS/Ativ2/SomaColuna.c
@@ -12,10 +12,8 @@ Saída: M números inteiros, representando a soma dos elementos de cada coluna.
 #include <stdio.h>
 #include <time.h>
 int n, m;
-void lerMatriz(int n, int m, int mat[n][m])
-{
-  for (int i = 0; i < n; i++)
-  {
+void lerMatriz(int n, int m, int mat[n][m]){
+  for (int i = 0; i < n; i++){
     for (int j = 0; j < m; j++)
     {
       scanf("%d", &mat[i][j]);
@@ -23,28 +21,23 @@ void lerMatriz(int n, int m, int mat[n][m])
   }
 }
 
-void SumCol(int n, int m, int Mat[n][m], int Vet[m])
-{
+void SumCol(int n, int m, int Mat[n][m], int Vet[m]){
   int soma = 0;
-  for (int j = 0; j < m; j++)
-  {
-    for (int i = 0; i < n; i++)
-    {
+  for (int j = 0; j < m; j++){
+    for (int i = 0; i < n; i++){
       soma += Mat[i][j];
     }
     Vet[j] = soma;
     soma = 0;
   }
-  int main()
-  {
-
+}
+  int main(){
     scanf("%d %d", &n, &m);
     int Mat[n][m];
     lerMatriz(n, m, Mat);
     int Vet[m];
     SumCol(n, m, Mat, Vet);
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++){
       printf("%d \t", Vet[i]);
     }
     return 0;
