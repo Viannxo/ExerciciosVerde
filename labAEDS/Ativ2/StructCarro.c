ccfa7acbdaf7211b8e63dc7f1cbf416175549c83
Viannxo <eduardo.vmeira@gmail.com>
2024-11-11 15:21:38 -0300

    edit

--- labAEDS/Ativ2/StructCarro.c
+++ labAEDS/Ativ2/StructCarro.c
@@ -45,8 +45,8 @@ typedef struct carro{
     float preco;
 }Carro;
 
-Carro CadastroCarros(int n, Carro car){
-    Carro car[n];
+Carro CadastroCarros(int n, Carro car[]){
+    car[n];
     for(int i=0;i<n;i++){
         printf("Digite a marca do carro:\n");
         scanf("%s", car[i].marca);
