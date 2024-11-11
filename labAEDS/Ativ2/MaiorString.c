dfaf616a92dc8e609202a0a5bc9c78982a87ba94
Viannxo <eduardo.vmeira@gmail.com>
2024-11-11 19:23:14 -0300

    add e edit

--- labAEDS/Ativ2/MaiorString.c
+++ labAEDS/Ativ2/MaiorString.c
@@ -1,44 +1,52 @@
-/* Escreva um programa em C que leia um número NNN de strings,
-conforme determinado pelo usuário. O programa deve então imprimir
-a maior string em termos de comprimento (tamanho) e a maior string 
-em termos lexicográficos (ordem alfabética).
-
-Entrada:
-A primeira linha contém um número inteiro N, representando a quantidade de strings a serem lidas.
-As N linhas seguintes contêm uma string em cada uma.
-
-Saída: :
-A primeira linha deve conter a maior string em termos de comprimento.
-A segunda linha deve conter a maior string em termos lexicográficos.
-
-Ex-> Entrada: 3
-banana
-maçã
-uva
-
-Saida:
-banana
-uva
-*/
-
 #include <stdio.h>
-#include <string.h>
 
-int main(){
+int strTamanho(char str[]) {
+    int count = 0;
+    while (str[count] != '\0') {
+        count++;
+    }
+    return count;
+}
+
+int strLexi(char str1[], char str2[]) {
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] > str2[i]) return 1;
+        else if (str1[i] < str2[i]) return -1;
+        i++;
+    }
+    if (str1[i] == '\0' && str2[i] == '\0') return 0;
+    else if (str1[i] == '\0') return -1;
+    else return 1;
+}
+
+int main() {
     int n;
     scanf("%d", &n);
-    char stg[n][100];
-    for(int i = 0; i < n; i++){
-        scanf("%s", &stg[i]);
-    }
-    int maior = strlen(stg[0]);
-    int posicao = 0;
-    for(int i = 1; i < n; i++){
-        if(strlen(stg[i]) > maior){
-            maior = strlen(stg[i]);
-            posicao = i;
+
+    char strMaiorTam[100] = "";
+    char strMaiorLexi[100] = "";
+    int tam = 0;
+
+    for (int i = 0; i < n; i++) {
+        char str[100];
+        scanf("%s", str);
+
+        if (strTamanho(str) > tam) {
+            tam = strTamanho(str);
+            for (int k = 0; k < 100; k++) {
+                strMaiorTam[k] = str[k];
+            }
+        }
+
+        if (strLexi(str, strMaiorLexi) > 0) {
+            for (int j = 0; j < 100; j++) {
+                strMaiorLexi[j] = str[j];
+            }
         }
     }
-    printf("%s\n", stg[posicao]);
+
+    printf("%s\n", strMaiorTam);
+    printf("%s\n", strMaiorLexi);
     return 0;
 }
